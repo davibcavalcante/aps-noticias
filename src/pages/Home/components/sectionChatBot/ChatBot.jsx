3bec4f7b64f44f5eeacec75cb69a457940605a65
Davi <davibcd@gmail.com>
2025-03-23 19:52:46 -0300

    Develop: ajustando seção do chatbot

--- src/pages/Home/components/sectionChatBot/ChatBot.jsx
+++ src/pages/Home/components/sectionChatBot/ChatBot.jsx
@@ -2,24 +2,22 @@ import { Send } from "lucide-react";
 
 const ChatBot = () => {
   return (
-    <section className=" flex justify-center items-center w-1/2">
-      <section className="bg-secondary flex flex-col w-10/12 h-10/12 rounded-xl p-4 shadow-lg">
-        <section className="flex-1 relative overflow-y-auto">
-          <div className="sticky top-0 left-1/2">
-            <h1 className="text-center font-poppins text-white text-2xl">Seja Bem-Vindo</h1>
-            <h2 className="text-center font-poppins font-light text-white tracking-widest">IA de notícias</h2>
-          </div>
-        </section>
-        <section className="">
-          <form>
-            <section className="flex items-center">
-              <input type="text" placeholder="Faça uma pergunta sobre notícias" className="text-white placeholder:text-white border rounded-sm py-2 px-4 flex-1" />
-              <div className="w-16 h-16 flex items-center justify-center">
-                <Send size={30} className="text-white" />
-              </div>
-            </section>
-          </form>
-        </section>
+    <section className="h-full flex flex-col">
+      <section className="flex-1 relative overflow-y-auto">
+        <div className="sticky top-0 left-1/2">
+          <h1 className="text-center font-poppins text-white text-2xl">Seja Bem-Vindo</h1>
+          <h2 className="text-center font-poppins font-light text-white tracking-widest">IA de notícias</h2>
+        </div>
+      </section>
+      <section className="">
+        <form>
+          <section className="flex items-center">
+            <input type="text" placeholder="Faça uma pergunta sobre notícias" className="text-white placeholder:text-white border rounded-sm py-2 px-4 flex-1" />
+            <div className="w-16 h-16 flex items-center justify-center">
+              <Send size={30} className="text-white" />
+            </div>
+          </section>
+        </form>
       </section>
     </section>
   );
