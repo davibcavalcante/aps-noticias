3342c819aca6582982dd2b93ff050974aa3d8b41
Davi <davibcd@gmail.com>
2025-03-23 19:56:24 -0300

    Develop: ajustes de infra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Home from './pages/Home';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx'
+import Home from './pages/Home/';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
